a5be0ebab5e61279628c4e4b76cb80130022816d
Kimskaya <120120877+Kimskaya@users.noreply.github.com>
2023-10-14 06:34:24 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -10,25 +10,6 @@ import pathlib
 
 __all__ = ['sort_files']
 
-# def sort_file(path):
-#     os.chdir(path)
-#     ext = {}
-#     for i in path.iterdir():
-#         if i.is_file():
-#             ext[i.suffix] = ext.get(i.suffix, []) + [i]
-#     for key, value in ext.items():
-#         os.mkdir(key)
-#         for file in value:
-#             try:
-#                 file.replace(path/key/file.name)
-#             except PermissionError:
-#                 continue
-
-
-# sort_file(pathlib.Path(r'D:\nika\учеба-GeekBrains\GIT_repositorys\repoz_python_seminar_5_26_09_23\packeg_seminar7\test'))      
-
-
-# 2 
 
 def sort_files(path):
     os.chdir(path)
